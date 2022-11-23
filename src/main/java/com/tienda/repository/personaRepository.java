95a3a929a3f8c3155ca864d178a05cb249972f05
AxelMCS1 <axelcmacho10@gmail.com>
2022-11-22 22:15:09 -0600

    Clase postman

--- src/main/java/com/tienda/repository/personaRepository.java
+++ src/main/java/com/tienda/repository/personaRepository.java
@@ -5,13 +5,18 @@
 package com.tienda.repository;
 
 import com.tienda.entity.Persona;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author axelc
  */
+@Repository
 public interface personaRepository extends CrudRepository<Persona, Long>{
+    
+    @Query(value = "Select * from persona where apellido1 = 'Lima'", nativeQuery = true)
     
     Persona findByNombre (String nombre);
 }
